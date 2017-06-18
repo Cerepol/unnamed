d3e503914b4794e6a7e29b2d7eb539159af5a67d
Cerepol <cerepol@gmail.com>
2017-06-18 18:04:31 -0400

    Revert "Revert "Fireball and Targetting""
    This reverts commit b2689b97693ad8e91598de893dfe103d302c3a68.

--- test.py
+++ test.py
@@ -17,7 +17,28 @@ terminal.printf(1, 10, "[color=orange]Welcome[/color][color=red] to the lang zon
 
 terminal.refresh()
 
+x = 10
+y = 12
+effected_points = [(x + dx, y + dy) for (dx, dy) in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, +1), (+1, -1), (+1, 0), (+1, +1))]
+print (effected_points)
+print(str(effected_points[0][0]) + " " + str(effected_points[0][1]))
+print(effected_points[-1])
+
+
+
+terminal.layer(20)
+
+terminal.color("purple")
+terminal.put(x, y, '*')
+
+terminal.color("orange")
+for (x, y) in effected_points:
+	terminal.put(x, y, "*")
+
+terminal.refresh()
+
 while terminal.read() != terminal.TK_ESCAPE:
 	pass
  
 terminal.close()
+
